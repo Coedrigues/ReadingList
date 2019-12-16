d8ac9eb7875db14f178996639847c43b206e59f3
EC2 Default User <ec2-user@ip-172-31-86-183.ec2.internal>
2019-12-16 18:05:19 +0000

    Teste 16-12-19

--- test/integration/listing_books_test.rb
+++ test/integration/listing_books_test.rb
@@ -5,6 +5,7 @@ class ListingBooksTest < ActionDispatch::IntegrationTest
     Book.create!(title: 'Pragmatic Programmer', rating: 5)
     Book.create!(title: "Ender's Game", rating: 5)
   end
+  
   test 'listing books' do
     get '/books'
     
@@ -12,5 +13,14 @@ class ListingBooksTest < ActionDispatch::IntegrationTest
     assert_equal Mime::JSON, response.content_type
     
     assert_equal Book.count, JSON.parse(response.body).size
+  end
+  
+  test 'lists top rated books' do
+    get '/books?rating=5'
+    
+    assert_equal 200, reponse.status
+    assert_equal Mime::JSON, reponse.content_type
+    
+    assert_equal 1, JSON.parse(reponse.body).size
   end
 end
